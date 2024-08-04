391283764510f58f764f8712ee3aec3b2509a22f
Tobias Waggoner <tobias.waggoner@gmail.com>
2024-08-04 17:23:58 +0200

    feat: removed old version

--- src/unity/Virtual Museum/Assets/DemoMarkerSkybox.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DemoMarkerSkybox : MonoBehaviour
-{
-    public void LoadDemoSkyBox(){
-        GameObject skyboxObject = GameObject.FindGameObjectWithTag("3DImageSphere").transform.GetChild(0).gameObject;
-        skyboxObject.SetActive(true);
-    }
-}
